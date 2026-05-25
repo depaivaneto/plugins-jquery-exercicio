af6afd3fa6adf6c5b090aaa6a9ae3c1ae4dd1098
depaivaneto <nluiz0478@gmail.com>
2026-05-25 16:12:20 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,40 @@
-acscdsfssf
+$(document).ready(function () {
+
+    // Máscaras
+    $('#telefone').mask('(00) 00000-0000');
+    $('#cpf').mask('000.000.000-00');
+    $('#cep').mask('00000-000');
+
+    // Impedir números no nome
+$('#nome').on('input', function () {
+    this.value = this.value.replace(/[0-9]/g, '');
+});
+
+
+    // Carrossel
+    $('.carousel').slick({
+        autoplay: true,
+        arrows: false,
+        dots: true
+    });
+
+    // Validação
+    $('#form').validate({
+        rules: {
+            nome: { required: true },
+            email: { required: true, email: true },
+            telefone: { required: true },
+            cpf: { required: true },
+            endereco: { required: true },
+            cep: { required: true }
+        },
+        submitHandler: function (form) {
+            alert('Sua requisição foi enviada para análise, parabéns pela aquisição!');
+            form.reset();
+        },
+        invalidHandler: function () {
+            alert('Por favor, preencha todos os campos corretamente.');
+        }
+    });
+
+});
